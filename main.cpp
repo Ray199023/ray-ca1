7ef293f8211a12e01afcff1e355eada5e9934fcd
Ray199023 <raykierans@gmail.com>
2023-07-22 15:18:11 +0000

    First commit
    Signed-off-by: Ray199023 <raykierans@gmail.com>

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include "mbed.h"
+
+int main() {
+    // initialize variables
+    int a = 10;
+    int b = 20;
+    int c = 30;
+    int d, e;
+
+    // add values of a and b and store it in variable d
+    d = a + b;
+
+    // print the value of variable d
+    printf("Value of variable d: %d\n", d);
+
+    // subtract variable c from d and store in variable e
+    e = d - c;
+
+    // print the value of variable e
+    printf("Value of variable e: %d\n", e);
+
+    return 0;
+}
+
+
+
+
+
